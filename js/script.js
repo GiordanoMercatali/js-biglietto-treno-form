d75bca3fbd3d09285f016084aa206d14bb0c155e
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2023-10-19 18:26:53 +0200

    add: a touch of style to the website

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ console.log(travelDistanceElem);
 const userAgeElem = document.getElementById("age");
 console.log(userAgeElem);
 
+const resultSection = document.querySelector(".result-section");
+
 const sendBtn = document.getElementById("send");
 sendBtn.addEventListener("click", function(){
     const travelDistance = travelDistanceElem.value;
@@ -38,14 +40,23 @@ sendBtn.addEventListener("click", function(){
 
     console.log(travelFinalCost.toFixed(2));
 
+    function getRandomNumber(max) {
+        return Math.floor(Math.random() * max);
+      }
+    
+    let ticketCode = getRandomNumber(99999);
+
     let costReturn = "";
 
      if (userAge >= 18 && userAge <= 65){
-         costReturn = `The cost of your travel is ${travelFinalCost.toFixed(2)}€`;
-     } else if(userAge < 18){
-         costReturn = `Minors get a ${discountAmount.toFixed(2)}€ discount. The cost of your travel is ${travelFinalCost.toFixed(2)}€`;
-     } else{
-         costReturn = `Old people get a ${discountAmount.toFixed(2)}€ discount. The cost of your travel is ${travelFinalCost.toFixed(2)}€`;
+         costReturn = `The cost of your travel is ${travelFinalCost.toFixed(2)}€
+         <br>The code of your ticket is ${ticketCode}`;
+        } else if(userAge < 18){
+         costReturn = `Minors get a ${discountAmount.toFixed(2)}€ discount. The cost of your travel is ${travelFinalCost.toFixed(2)}€
+         <br>The code of your ticket is ${ticketCode}`;
+        } else{
+         costReturn = `Elders get a ${discountAmount.toFixed(2)}€ discount. The cost of your travel is ${travelFinalCost.toFixed(2)}€
+         <br>The code of your ticket is ${ticketCode}`;
      }
 
     // if (userAge === "18 - 65"){
@@ -56,6 +67,8 @@ sendBtn.addEventListener("click", function(){
     //     costReturn = `Old people get a ${discountAmount.toFixed(2)}€ discount. The cost of your travel is ${travelFinalCost.toFixed(2)}€`;
     // }
 
+    resultSection.classList.remove("d-none");
+    resultSection.classList.add("d-block");
     document.getElementById("result").innerHTML = costReturn;
 });
 
@@ -64,5 +77,7 @@ clearBtn.addEventListener("click", function(){
     travelDistanceElem.value = "";
     userAgeElem.value = "";
 
+    resultSection.classList.remove("d-block");
+    resultSection.classList.add("d-none");
     document.getElementById("result").innerHTML = "";
 });
